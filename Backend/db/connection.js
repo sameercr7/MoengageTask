43d6d8ee07d39959b0d3bf4296833eb0b87f4428
sameer_cr7 <sameer05072001@gmail.com>
2023-12-05 17:01:19 +0530

    V1

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-
-//  const URL_local = "mongodb://127.0.0.1:27017/breweryDB";
- const URL_cloud =
- "mongodb+srv://admin-chintu:Ek69bP9p7iwtRJO1@cluster0.zaseysf.mongodb.net/breweryDB?retryWrites=true&w=majority";
-// const URL_cloud = "mongodb+srv://sam101:J3COlpHSaqeXDzw7@cluster0.m7dtnnn.mongodb.net/breweryDB?retryWrites=true&w=majority
+require("dotenv").config();
 
 const connect = async () => {
+  const URL_cloud = process.env.DB_URL;
   await mongoose.connect(URL_cloud);
 };
 
